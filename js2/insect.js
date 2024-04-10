c20403c444037cb383b0409f668ce95553ec9ffa
Carlos Ruvalcaba <142750480+Carlos7344@users.noreply.github.com>
2024-04-10 15:53:32 +0000

    Wed, Apr 10, 2024, 8:53 AM -07:00

--- js2/insect.js
+++ js2/insect.js
@@ -1,3 +1,11 @@
 const screens = document.querySelectorAll('.screen')
+const choose_insect_btns = document.querySelector('.choose-insect-btn')
+const game_container = document.getElementById('game-container')
+const start_btn = document.getElementById('start-btn')
+const timeEl = document.getElementById('time')
+const scoreEl = document.getElementById('score')
+const message = document.getElementById('message')
+let seconds = 0
+let score = 0
+let selected_insect = {}
 
-screens[0].classlist.add('up')
