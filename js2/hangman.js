f919af44939e5b342edaaf991ca9175430070a80
Carlos Ruvalcaba <142750480+Carlos7344@users.noreply.github.com>
2024-04-23 17:14:07 +0000

    Tue, Apr 23, 2024, 10:14 AM -07:00

--- js2/hangman.js
+++ js2/hangman.js
@@ -6,7 +6,7 @@ const notification = document.getElementById('notification-container')
 const finalMessage = document.getElementById('final-message')
 const figureParts = document.querySelectorAll('.figure-part')
 
-const word = ['application', 'programming', 'interface', 'wizard']
+const word = ['application', 'programming', 'interface', 'wizard', 'Python', 'html', 'sql', 'marcel']
 
 let selectedIndex = Math.floor(word.length * Math.random())
 let selectedWord = word[selectedIndex]
