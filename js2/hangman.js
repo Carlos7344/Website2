d6569ffee828b5efe7b9c8e2268069849049ae97
Carlos Ruvalcaba <142750480+Carlos7344@users.noreply.github.com>
2024-04-23 16:57:27 +0000

    Tue, Apr 23, 2024, 9:57 AM -07:00

--- js2/hangman.js
+++ js2/hangman.js
@@ -1,6 +1,6 @@
 const worldEl = document.getElementById('word')
 const wrongLettersEl = document.getElementById('wrong-letters')
-const playAgainBtn = document.getElementById('play-again')
+const playAgainBtn = document.getElementById('play-button')
 const popup = document.getElementById('popup-container')
 const notification = document.getElementById('notification-container')
 const finalMessage = document.getElementByid('final-message')
