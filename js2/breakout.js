ad00b2ada18e0ef9089ed9eb291b8e2840312e0a
Carlos Ruvalcaba <142750480+Carlos7344@users.noreply.github.com>
2024-05-20 05:38:11 +0000

    Sun, May 19, 2024, 10:38 PM -07:00

--- js2/breakout.js
+++ js2/breakout.js
@@ -245,21 +245,21 @@ closeBtn.addEventListener('click', () => {
 
  let gameStarted = false
 
-document.addEventListener('keydown', function(event) {
-    if (!gameStarted) {
-        gameStarted = true
-        update()
-    }
-})
+ document.addEventListener('keydown', function(event) {
+     if (!gameStarted) {
+         gameStarted = true
+         drawBricks() // Draw bricks when the game starts
+         update()
+     }
+ })
 
-function update() {
-    moveBall()
-    movePaddle()
-    draw()
-    requestAnimationFrame(update)
-    if (ball.y + ball.size > canvas.height) {
-        ball.dy = -1 * ball.dy
-        return 0
-    }
-    drawBricks() // Draw bricks continuously
-}
+ function update() {
+     moveBall()
+     movePaddle()
+     draw()
+     requestAnimationFrame(update)
+     if (ball.y + ball.size > canvas.height) {
+         ball.dy = -1 * ball.dy
+         return 0
+     }
+ }
